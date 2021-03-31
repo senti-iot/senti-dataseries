d7943cfa56f7dd2b3f6665efa480791f08d26b6c
Andrei <at@senti.dk>
2021-03-31 18:02:14 +0200

    remove unnecesary console log

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -5,7 +5,7 @@ if [[ "$1" == "master" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/dataseries/production
 	systemctl restart senti-dataseries.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Dataseries MASTER updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Dataseries *master* updated."}' $2
 	echo
 	exit 0
 fi
@@ -14,7 +14,7 @@ if [[ "$1" == "dev" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/dataseries/development
 	systemctl restart senti-dataseries-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Dataseries DEV updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Dataseries *dev* updated."}' $2
 	echo
 	exit 0
 fi
