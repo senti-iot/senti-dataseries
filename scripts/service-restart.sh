8090283d39033a271666521cb2b48d0dc516df82
Andrei <at@senti.dk>
2021-03-31 19:46:51 +0200

    Service Restart update

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -5,7 +5,36 @@ if [[ "$1" == "master" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/dataseries/production
 	systemctl restart senti-dataseries.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Dataseries *master* updated."}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Senti Dataseries updated",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Branch:*\n'$1'"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Travis",
+					"emoji": true
+				},
+				"value": "travis-link",
+				"url": "http://travis-ci.com/github/senti-iot/senti-dataseries",
+				"action_id": "button-action"
+			}
+		}
+	]
+}' $2
 	echo
 	exit 0
 fi
@@ -28,7 +57,7 @@ if [[ "$1" == "dev" ]]; then
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": "*Branch:*\n"'"$1"'
+				"text": "*Branch:*\n'$1'"
 			},
 			"accessory": {
 				"type": "button",
